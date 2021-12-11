0a1986f85fe828755c44ec66c99b69534456c671
lucas <lucasportellaagu@gmail.com>
2021-12-11 20:00:09 -0300

    req 5

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,11 +1,10 @@
 import requests
 from parsel import Selector
 from time import sleep
-from pymongo import MongoClient
 from tech_news.Tecmundo_scraper import Scrap_tecmundo
+from tech_news.database import create_news
 
 
-# Requisito 1
 # https://softbranchdevelopers.com/python-requests-library-exception-handling-advanced-request-get-parameters/#all-exceptions
 def fetch(url):
     resp = ""
@@ -20,14 +19,12 @@ def fetch(url):
         return None
 
 
-# Requisito 2
 def scrape_noticia(html_content):
     selector = Selector(text=html_content)
     scraped_news = Scrap_tecmundo(selector)
     return scraped_news.mount()
 
 
-# Requisito 3
 # header Notícias have more hidden news with class .tec--card__title__link, so
 #  use id #js-main to restrict to only main news
 def scrape_novidades(html_content):
@@ -38,22 +35,20 @@ def scrape_novidades(html_content):
     return fresh_news
 
 
-# Requisito 4
 def scrape_next_page_link(html_content):
     selector = Selector(text=html_content)
     next_page = selector.css("#js-main .tec--btn::attr(href)").get()
     return next_page
 
 
-# Requisito 5
 def add_news_to_database(requested_news):
-    client = MongoClient()
-    db = client.tech_news
+    news_to_insert = []
     for url in requested_news:
         html_content = fetch(url)
         new = scrape_noticia(html_content)
-        db.news.insert_one(new)
-        # needs to return inserted news
+        news_to_insert.append(new)
+    create_news(news_to_insert)
+    return news_to_insert
 
 
 def get_tech_news(amount):
@@ -70,14 +65,3 @@ def get_tech_news(amount):
         latest_news.extend(scrape_novidades(html_content))
     requested_news = latest_news[:amount]
     return add_news_to_database(requested_news)
-
-
-get_tech_news(7)
-
-
-# //////////////////////////////
-# content = fetch('https://www.tecmundo.com.br/mobilidade-urbana-smart-cities/
-# 155000-musk-tesla-carros-totalmente-autonomos.htm')
-
-# musk = scrape_noticia(content)
-# print(musk)
