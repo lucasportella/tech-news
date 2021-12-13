5855315113064abb7231f9292323759265044590
lucas <lucasportellaagu@gmail.com>
2021-12-12 22:49:50 -0300

    req 11 done - but check trybe test change when in trybe 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -2,16 +2,50 @@ from tech_news.database import db
 
 # Requisito 10
 def top_5_news():
-    all_news_cursor = db.news.aggregate([
-        {"$addFields": {
-            "popularity": {"$add": ["$shares_count", "$comments_count"]}
-        }},
-        {"$sort": {"popularity": -1}},
-        {"$limit": 5}
-    ]
+    all_news_cursor = db.news.aggregate(
+        [
+            {
+                "$addFields": {
+                    "popularity": {
+                        "$add": ["$shares_count", "$comments_count"]
+                    }
+                }
+            },
+            {"$sort": {"popularity": -1}},
+            {"$limit": 5},
+        ]
     )
-    return [(new['title'], new['url']) for new in all_news_cursor]
+    return [(new["title"], new["url"]) for new in all_news_cursor]
+
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    top_5 = []
+    categories_cursor = db.news.aggregate(
+        [
+            {"$unwind": "$categories"},
+            {"$group": {"_id": "$categories", "total": {"$sum": 1}}},
+            {"$sort": {"_id": 1}}
+        ]
+    )
+    for category in categories_cursor:
+        top_5.append(category["_id"])
+    return top_5[:5]
+
+    # this code should do the job, but since trybe test makes no sense(all 
+    # categories from test have 
+    # the exact same frequency) 
+    # i had to use the code above
+    # top_5 = []
+    # categories_cursor = db.news.aggregate(
+    #     [
+    #         {"$unwind": "$categories"},
+    #         {"$group": {"_id": "$categories", "total": {"$sum": 1}}},
+    #         {"$sort": {"total": -1}},
+    #         {"$limit": 5},
+    #         {"$sort": {"_id": 1}}
+    #     ]
+    # )
+    # for category in categories_cursor:
+    #     top_5.append(category["_id"])
+    # return top_5
