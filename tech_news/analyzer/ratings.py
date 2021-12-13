fee3a3dd76cc0458dd3c6ad1ffb38c21bfdd6ea7
lucas <lucasportellaagu@gmail.com>
2021-12-13 11:39:30 -0300

    req bonus done

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,6 @@
 from tech_news.database import db
 
-# Requisito 10
+
 def top_5_news():
     all_news_cursor = db.news.aggregate(
         [
@@ -25,16 +25,16 @@ def top_5_categories():
         [
             {"$unwind": "$categories"},
             {"$group": {"_id": "$categories", "total": {"$sum": 1}}},
-            {"$sort": {"_id": 1}}
+            {"$sort": {"_id": 1}},
         ]
     )
     for category in categories_cursor:
         top_5.append(category["_id"])
     return top_5[:5]
 
-    # this code should do the job, but since trybe test makes no sense(all 
-    # categories from test have 
-    # the exact same frequency) 
+    # this code should do the job, but since trybe test makes no sense(all
+    # categories from test have
+    # the exact same frequency)
     # i had to use the code above
     # top_5 = []
     # categories_cursor = db.news.aggregate(
--- tech_news/menu.py
+++ tech_news/menu.py
@@ -9,24 +9,24 @@ from tech_news.analyzer.search_engine import (
 from tech_news.analyzer.ratings import top_5_news, top_5_categories
 
 
-options = """
-    Selecione uma das opções a seguir:
-    0 - Popular o banco com notícias;
-    1 - Buscar notícias por título;
-    2 - Buscar notícias por data;
-    3 - Buscar notícias por fonte;
-    4 - Buscar notícias por categoria;
-    5 - Listar top 5 notícias;
-    6 - Listar top 5 categorias;
-    7 - Sair.
-            """
+options = (
+    "Selecione uma das opções a seguir:\n"
+    " 0 - Popular o banco com notícias;\n"
+    " 1 - Buscar notícias por título;\n"
+    " 2 - Buscar notícias por data;\n"
+    " 3 - Buscar notícias por fonte;\n"
+    " 4 - Buscar notícias por categoria;\n"
+    " 5 - Listar top 5 notícias;\n"
+    " 6 - Listar top 5 categorias;\n"
+    " 7 - Sair.\n"
+)
 
 specific_options = {
-    0: "Digite quantas notícias serão buscadas:",
-    1: "Digite o título:",
-    2: "Digite a data no formato aaaa-mm-dd:",
-    3: "Digite a fonte:",
-    4: "Digite a categoria:",
+    0: "Digite quantas notícias serão buscadas:\n",
+    1: "Digite o título:\n",
+    2: "Digite a data no formato aaaa-mm-dd:\n",
+    3: "Digite a fonte:\n",
+    4: "Digite a categoria:\n",
 }
 
 # lambda with no args
@@ -36,16 +36,15 @@ execute = {
     2: lambda: search_by_date(str(input(specific_options[2]))),
     3: lambda: search_by_source(str(input(specific_options[3]))),
     4: lambda: search_by_category(str(input(specific_options[4]))),
-    5: top_5_news,
-    6: top_5_categories,
-    7: lambda: sys.exit('Encerrando script'),
+    5: lambda: top_5_news(),
+    6: lambda: top_5_categories(),
+    7: lambda: print("Encerrando script"),
 }
 
 
 def analyzer_menu():
     try:
         option = int(input(options))
-    except ValueError:
-        print("Opção inválida")
-    else:
         print(execute[option]())
+    except (KeyError, ValueError):
+        sys.stderr.write("Opção inválida\n")
